e8658f3bc2b9be2ae51a67d37eff063a44941464
Alexander D <alex.hh@me.com>
2017-04-06 17:50:57 +0300

    updated main module

--- lib/csvdumper.rb
+++ lib/csvdumper.rb
@@ -1,5 +1,14 @@
 require "csvdumper/version"
+require "csvdumper/row_parser"
+require "csvdumper/importer"
 
 module Csvdumper
-  # Your code goes here...
+
+  if defined?(Rails::Railtie)
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load File.expand_path('../tasks/import.rake', __FILE__)
+      end
+    end
+  end
 end
--- spec/csvdumper_spec.rb
+++ spec/csvdumper_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Csvdumper do
   it "has a version number" do
     expect(Csvdumper::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
